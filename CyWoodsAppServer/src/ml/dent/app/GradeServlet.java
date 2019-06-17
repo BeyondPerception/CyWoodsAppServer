ef7bbda59aae9f1bb0dacebef69947ce061c6dc0
Ronak Malik <ronakmalik4242@gmail.com>
2019-06-16 20:50:44 -0500

    Setup maven project

---
+++ CyWoodsAppServer/src/ml/dent/app/GradeServlet.java
@@ -0,0 +1,30 @@
+package ml.dent.app;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/GradeServlet")
+public class GradeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public GradeServlet() {
+		super();
+	}
+
+	/**
+	 * Other methods exist such as doPost, doPut, and doDelete, but in this case
+	 * only GET requests are necessary.
+	 * 
+	 * The frontend will send an HTTP request to this servlet at the above endpoint
+	 * (e.g. www.dent.ml/GradeServlet), and we'll write our response to the HTTP
+	 * response.
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+}
