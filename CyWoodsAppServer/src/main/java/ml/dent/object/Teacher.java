95d59ffe82affd7e6b4c4810d7338bf472d49a51
Ronak Malik <ronakmalik4242@gmail.com>
2019-06-24 22:15:47 -0500

    Add Teacher object

---
+++ CyWoodsAppServer/src/main/java/ml/dent/object/Teacher.java
@@ -0,0 +1,39 @@
+package ml.dent.object;
+
+import ml.dent.json.JsonObject;
+
+public class Teacher {
+	private String name;
+	private String email;
+
+	public Teacher(String n, String e) {
+		setName(n);
+		setEmail(e);
+	}
+
+	public Teacher() {
+		name = "";
+		email = "";
+	}
+
+	public JsonObject getJsonData() {
+		return new JsonObject().add("name", name).add("email", email);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
