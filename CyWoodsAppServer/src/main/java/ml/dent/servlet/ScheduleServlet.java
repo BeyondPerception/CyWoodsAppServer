5cdba33aca15c2e3ebb735eb9f5bda83707e829a
Ronak Malik <ronakmalik4242@gmail.com>
2019-07-09 14:49:12 -0500

    Fix bug where no while loops ran because of forgotten bang

--- CyWoodsAppServer/src/main/java/ml/dent/servlet/ScheduleServlet.java
+++ CyWoodsAppServer/src/main/java/ml/dent/servlet/ScheduleServlet.java
@@ -1,9 +1,11 @@
 package ml.dent.servlet;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import javax.servlet.ServletException;
@@ -39,31 +41,47 @@ public class ScheduleServlet extends HttpServlet {
 	 * because of the way the school gives us the info about each day's schedule,
 	 * this is what we have to do)
 	 */
+	@SuppressWarnings("resource")
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		// Prepare writer for response
+		PrintWriter pw = response.getWriter();
+
+		// We want them to send us the day so it is more customizable for the client and
+		// we don't have to worry about setting our clock
+		String day = request.getParameter("day");
+		if (day == null) {
+			
+		}
+		
 		// This grounds the path in the real file
 		// system so we can use relative paths
 		// without the virtual context messing us up
-		String filePath = this.getServletContext().getRealPath("WEB-INF");
+		String filePath = "/home/ronak/Workspace/CyWoodsAppServer/MonthlySchedules.txt";// this.getServletContext().getRealPath("WEB-INF");
 		// The "../"s mean the previous
 		// directory; 3 of them mean
 		// to escape the server dir
 		// to find the schedule
 		// file.
-		filePath = "../../../" + filePath;
+//		filePath = "../../.." + filePath;
+//		System.out.println(filePath);
 
 		// Using a scanner rather than something faster like a buffered reader because
 		// the performance difference is not important, and we can choose to use regex
 		// if necessary
-		Scanner file = new Scanner(new File(filePath));
+		Scanner file = null;
+		try {
+			file = new Scanner(new File(filePath));
+		} catch (FileNotFoundException e) {
+			pw.println(Default.InternalServerError("SEVERE: Master schedule file missing"));
+			return;
+		}
 		// Change formatting from yyyy-mm-dd to mm/dd
 		String currentDay = LocalDate.now().toString().replaceAll("\\d{4}-(\\d{2})-(\\d{2})", "$1/$2");
 		// Move to the current date within the file; consumes characters
 		file.findWithinHorizon(currentDay, Integer.MAX_VALUE);
-
-		// Prepare writer for response
-		PrintWriter pw = response.getWriter();
+		file.nextLine(); // pick up new line char
 
 		JsonObject res = new JsonObject().add("date", currentDay);
 
@@ -88,12 +106,14 @@ public class ScheduleServlet extends HttpServlet {
 				res.add("type", "custom");
 				JsonObject shed = new JsonObject();
 				String nextLine;
-				while ((nextLine = file.nextLine()).equals("LUNCH")) {
+				// Reads all periods until lunch begins (usually p.1-p.3)
+				while (!(nextLine = file.nextLine()).equals("LUNCH")) {
 					String[] line = nextLine.split(" ");
 					shed.add(line[0], new JsonArray().add(line[1], line[2]));
 				}
+				// Parsing the periods that change because of lunch (usually p.4-p.5)
 				JsonObject lunch = new JsonObject();
-				while ((nextLine = file.nextLine()).equals("END")) {
+				while (!(nextLine = file.nextLine()).equals("END")) {
 					String[] line = nextLine.split(" ");
 					if (line.length == 1) {
 						lunch = new JsonObject();
@@ -102,7 +122,8 @@ public class ScheduleServlet extends HttpServlet {
 						lunch.add(line[0], new JsonArray().add(line[1], line[2]));
 					}
 				}
-				while ((nextLine = file.nextLine()).equals("END")) {
+				// Reading the rest of the day (usually p.6-p.7)
+				while (!(nextLine = file.nextLine()).equals("END")) {
 					String[] line = nextLine.split(" ");
 					shed.add(line[0], new JsonArray().add(line[1], line[2]));
 				}
