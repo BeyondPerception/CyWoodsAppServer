dda1d9b8190a9cf63d8e706fb4e456655c92ec41
Ronak Malik <ronakmalik4242@gmail.com>
2019-08-05 12:14:25 -0500

    Create NewsFetcher in web package

---
+++ CyWoodsAppServer/src/main/java/ml/dent/web/NewsFetcher.java
@@ -0,0 +1,6 @@
+package ml.dent.web;
+
+public class NewsFetcher {
+	private static final String SCHOOL_NEWS_URL = "https://cywoods.cf";
+	private static final String CRIMSON_CONNECTION_URL = "http://www.thecrimsonconnection.com/category/news/cy-woods/";
+}
