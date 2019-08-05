d16b2749b94d82dfcd0263ddb34bb0caf237dd3d
Ronak Malik <ronakmalik4242@gmail.com>
2019-08-05 12:14:08 -0500

    Add important URLs to NewsServlet

--- CyWoodsAppServer/src/main/java/ml/dent/servlet/NewsServlet.java
+++ CyWoodsAppServer/src/main/java/ml/dent/servlet/NewsServlet.java
@@ -1,11 +1,15 @@
 package ml.dent.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import ml.dent.util.Default;
 
 /**
  * Servlet implementation class NewsServlet
@@ -13,30 +17,37 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/News")
 public class NewsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public NewsServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public NewsServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		
+	}
+
+	/**
+	 *
+	 * We don't want to allow them to send POST reqs, but we'll handle them anyways
+	 * otherwise it will give them a huge, very unreadable error.
+	 * 
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		PrintWriter pw = response.getWriter();
+		pw.println(Default.BadGateway("POST reqs not allowed"));
 	}
 
 }
