90ca475c27959ffd993332217fa8b7480ad5ada1
Ronak Malik <ronakmalik4242@gmail.com>
2019-08-13 14:51:16 -0500

    Fix date formatting in AppNewsItem

--- CyWoodsAppServer/src/main/java/ml/dent/object/news/AppNewsItem.java
+++ CyWoodsAppServer/src/main/java/ml/dent/object/news/AppNewsItem.java
@@ -4,6 +4,6 @@ import java.text.SimpleDateFormat;
 
 public class AppNewsItem extends NewsItem {
 	public AppNewsItem(String title, String date) throws Exception {
-		super("App News", title, new SimpleDateFormat("MMMM d, y").parse(date));
+		super("App News", title, new SimpleDateFormat("MM/dd/YYYY").parse(date));
 	}
 }
