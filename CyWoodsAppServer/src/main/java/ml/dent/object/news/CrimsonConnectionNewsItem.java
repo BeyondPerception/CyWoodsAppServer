cad6d6811cc48a5ad0494bb7220778cf3ecb2829
Ronak Malik <ronakmalik4242@gmail.com>
2019-08-13 17:19:03 -0500

    Fix unncecessary processing in CrimsonConnectionNewsItem

--- CyWoodsAppServer/src/main/java/ml/dent/object/news/CrimsonConnectionNewsItem.java
+++ CyWoodsAppServer/src/main/java/ml/dent/object/news/CrimsonConnectionNewsItem.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 
 public class CrimsonConnectionNewsItem extends NewsItem {
 	public CrimsonConnectionNewsItem(String title, String date, String url) throws Exception {
-		super("Crimson Connection", title, new SimpleDateFormat("MMMM d y").parse(date.replaceAll("[\\.,]", "")), url,
-				1);
+		super("Crimson Connection", title, new SimpleDateFormat("MMMM dd, yyyy").parse(date), url, 1);
 	}
 }
