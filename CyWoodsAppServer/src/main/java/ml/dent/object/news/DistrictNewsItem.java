0ade15166b8c05b4b9033b8f091d79891264f1cb
Ronak Malik <ronakmalik4242@gmail.com>
2019-08-13 16:35:55 -0500

    Clean up unnecessary processing in DistrictNewsItem

--- CyWoodsAppServer/src/main/java/ml/dent/object/news/DistrictNewsItem.java
+++ CyWoodsAppServer/src/main/java/ml/dent/object/news/DistrictNewsItem.java
@@ -4,6 +4,6 @@ import java.text.SimpleDateFormat;
 
 public class DistrictNewsItem extends NewsItem {
 	public DistrictNewsItem(String title, String date, String url) throws Exception {
-		super("District News", title, new SimpleDateFormat("E dd MMM y HH").parse(date.substring(0, date.indexOf(":")).replaceAll("[\\.,]", "").trim()), url, 1);
+		super("District News", title, new SimpleDateFormat("MMM. dd, yyyy").parse(date), url, 1);
 	}
 }
