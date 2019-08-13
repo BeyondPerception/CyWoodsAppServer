398fb11cc20fad96333f65a7c18da01cab840a67
Ronak Malik <ronakmalik4242@gmail.com>
2019-08-13 13:42:46 -0500

    Fix endpoint config in FacultyServlet

--- CyWoodsAppServer/src/main/java/ml/dent/servlet/FacultyServlet.java
+++ CyWoodsAppServer/src/main/java/ml/dent/servlet/FacultyServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,7 @@ import ml.dent.web.FacultyFetcher;
 /**
  * Servlet implementation class FacultyServlet
  */
+@WebServlet("/Faculty")
 public class FacultyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
