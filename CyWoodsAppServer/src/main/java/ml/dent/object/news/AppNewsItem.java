9153cf7060a004647b835076f008fa35925d8802
Ronak Malik <ronakmalik4242@gmail.com>
2019-08-13 16:33:07 -0500

    Fix bug with casing on SimpleDateFormat

--- CyWoodsAppServer/src/main/java/ml/dent/object/news/AppNewsItem.java
+++ CyWoodsAppServer/src/main/java/ml/dent/object/news/AppNewsItem.java
@@ -4,6 +4,6 @@ import java.text.SimpleDateFormat;
 
 public class AppNewsItem extends NewsItem {
 	public AppNewsItem(String title, String date, int priority) throws Exception {
-		super("App News", title, new SimpleDateFormat("MM/dd/YYYY").parse(date), priority);
+		super("App News", title, new SimpleDateFormat("MM/dd/yyyy").parse(date), priority);
 	}
 }
