2d9f248b22c5364f15e528f8455b6ed5a5db05b7
Ronak Malik <ronakmalik4242@gmail.com>
2019-09-01 15:03:03 -0500

    Add variables to AthleticItem to match online schedule

--- CyWoodsAppServer/src/main/java/ml/dent/object/athletics/AthleticItem.java
+++ CyWoodsAppServer/src/main/java/ml/dent/object/athletics/AthleticItem.java
@@ -3,21 +3,25 @@ package ml.dent.object.athletics;
 import ml.dent.json.JsonObject;
 
 public class AthleticItem {
-	private String type;
+	private String sport;
 	private String opponent;
 	private String ourScore;
 	private String oppScore;
+	private String dateTime;
+	private String location;
 
-	public AthleticItem(String t, String o, String ourS, String oppS) {
-		type = t;
-		opponent = o;
+	public AthleticItem(String sp, String opp, String ourS, String oppS, String dT, String loc) {
+		sport = sp;
+		opponent = opp;
 		ourScore = ourS;
 		oppScore = oppS;
+		dateTime = dT;
+		location = loc;
 	}
 
 	public JsonObject getJsonData() {
-		return new JsonObject().add("type", type).add("opponent", opponent).add("ourScore", ourScore).add("oppScore",
-				oppScore);
+		return new JsonObject().add("sport", sport).add("opponent", opponent).add("ourScore", ourScore)
+				.add("oppScore", oppScore).add("dateTime", dateTime).add("location", location);
 	}
 
 }
