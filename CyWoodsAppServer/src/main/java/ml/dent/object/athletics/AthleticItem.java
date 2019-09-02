f055c970999c2818fb53a01cff5f694909c071a0
Ronak Malik <ronakmalik4242@gmail.com>
2019-09-02 10:52:29 -0500

    Reformat AthleticItem to better fit athletic schedule pages

--- CyWoodsAppServer/src/main/java/ml/dent/object/athletics/AthleticItem.java
+++ CyWoodsAppServer/src/main/java/ml/dent/object/athletics/AthleticItem.java
@@ -5,25 +5,25 @@ import ml.dent.json.JsonObject;
 public class AthleticItem {
 	private String sport;
 	private String opponent;
-	private String ourScore;
-	private String oppScore;
-	private String dateTime;
+	private String score;
+	private String date;
+	private String time;
 	private String location;
 	private String mapLink;
 
-	public AthleticItem(String sp, String opp, String ourS, String oppS, String dT, String loc, String link) {
+	public AthleticItem(String sp, String opp, String s, String d, String t, String loc, String link) {
 		sport = sp;
 		opponent = opp;
-		ourScore = ourS;
-		oppScore = oppS;
-		dateTime = dT;
+		score = s;
+		date = d;
+		time = t;
 		location = loc;
 		mapLink = link;
 	}
 
 	public JsonObject getJsonData() {
-		return new JsonObject().add("sport", sport).add("opponent", opponent).add("ourScore", ourScore)
-				.add("oppScore", oppScore).add("dateTime", dateTime).add("location", location).add("mapLink", mapLink);
+		return new JsonObject().add("sport", sport).add("opponent", opponent).add("score", score).add("date", date)
+				.add("time", time).add("location", location).add("mapLink", mapLink);
 	}
 
 }
