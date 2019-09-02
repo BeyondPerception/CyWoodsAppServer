021bcef87880c52a36162bcc4e8c5b84b47c9ebc
Ronak Malik <ronakmalik4242@gmail.com>
2019-09-02 11:00:47 -0500

    Add compareTo to AthleticItem so it can be sorted by date

--- CyWoodsAppServer/src/main/java/ml/dent/object/athletics/AthleticItem.java
+++ CyWoodsAppServer/src/main/java/ml/dent/object/athletics/AthleticItem.java
@@ -1,8 +1,13 @@
 package ml.dent.object.athletics;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
 import ml.dent.json.JsonObject;
 
-public class AthleticItem {
+public class AthleticItem implements Comparable<AthleticItem> {
 	private String sport;
 	private String opponent;
 	private String score;
@@ -24,6 +29,21 @@ public class AthleticItem {
 	public JsonObject getJsonData() {
 		return new JsonObject().add("sport", sport).add("opponent", opponent).add("score", score).add("date", date)
 				.add("time", time).add("location", location).add("mapLink", mapLink);
+	}
+
+	@Override
+	public int compareTo(AthleticItem o) {
+		Date first;
+		Date second;
+		try {
+			first = new SimpleDateFormat("MM/dd/yy").parse(date);
+			second = new SimpleDateFormat("MM/dd/yy").parse(o.date);
+		} catch (ParseException e) {
+			first = new Date();
+			second = new Date();
+		}
+
+		return first.compareTo(second);
 	}
 
 }
