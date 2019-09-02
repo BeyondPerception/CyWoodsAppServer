c92362e5fd3d8afda23ed87849fcef9ce3c9f2dc
Ronak Malik <ronakmalik4242@gmail.com>
2019-09-02 11:11:00 -0500

    Add AthleticServlet to serve requests for AthleticFetcher

---
+++ CyWoodsAppServer/src/main/java/ml/dent/servlet/AthleticServlet.java
@@ -0,0 +1,72 @@
+package ml.dent.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ml.dent.json.JsonArray;
+import ml.dent.json.JsonObject;
+import ml.dent.object.athletics.AthleticItem;
+import ml.dent.util.Default;
+import ml.dent.web.AthleticsFetcher;
+
+/**
+ * Servlet implementation class AthleticServlet
+ */
+@WebServlet("/Athletics")
+public class AthleticServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AthleticServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter pw = response.getWriter();
+
+		AthleticsFetcher af = new AthleticsFetcher();
+		af.populateGames();
+
+		Vector<AthleticItem> items = af.getGames();
+
+		JsonObject jO = new JsonObject();
+
+		JsonArray ja = new JsonArray();
+
+		for (AthleticItem ai : items) {
+			ja.add(ai.getJsonData());
+		}
+
+		jO.add("games", ja);
+
+		pw.println(jO.format());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter pw = response.getWriter();
+
+		pw.println(Default.BadRequest("POST reqs not allowed!"));
+	}
+
+}
