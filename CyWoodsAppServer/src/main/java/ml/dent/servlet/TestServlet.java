2cdd79df21aa6b9524e23dc7986b814e0c1ee5fc
Ronak Malik <ronakmalik4242@gmail.com>
2019-09-08 21:57:04 -0500

    Fix loader.io test servlet

--- CyWoodsAppServer/src/main/java/ml/dent/servlet/TestServlet.java
+++ CyWoodsAppServer/src/main/java/ml/dent/servlet/TestServlet.java
@@ -9,13 +9,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementation class TestServlet
+ */
 @WebServlet("/loaderio-40a8b5b6364b4626504225e48efa22c0")
 public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter pw = resp.getWriter();
-		pw.println("loaderio-40a8b5b6364b4626504225e48efa22c0");
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TestServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.print("loaderio-40a8b5b6364b4626504225e48efa22c0");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
