57afc374293ddea121c0593985cdc08db19abf75
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-05 14:24:21 -0500

    Make creating the logging dir into a static block

--- CyWoodsAppServer/src/main/java/ml/dent/util/Logger.java
+++ CyWoodsAppServer/src/main/java/ml/dent/util/Logger.java
@@ -6,68 +6,67 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 
 /**
- * 
  * This is a system wide student logger that logs errrors
- * 
+ *
  * @author Ronak Malik
  */
 public class Logger {
 
-	private static final String LOG_DIR = "/efs/logs/";
-	private String logDir;
+    private static final String LOG_DIR = "/efs/logs/";
+    private              String logDir;
 
-	private static int count = 0;
+    private static int count = 0;
 
-	private PrintWriter pw;
+    private PrintWriter pw;
 
-	private static String currentDate;
+    private static String currentDate;
 
-	private File logFile;
+    private File logFile;
 
-	{
-		File logDir = new File(LOG_DIR);
-		if (!logDir.exists()) {
-			logDir.mkdirs();
-		}
-	}
+    static {
+        File logDir = new File(LOG_DIR);
+        if (!logDir.exists()) {
+            logDir.mkdirs();
+        }
+    }
 
-	public Logger(String internalDir) {
-		currentDate = LocalDate.now().toString();
-		logDir = LOG_DIR + internalDir + "/";
-		logFile = new File(logDir + currentDate + "-" + count);
-		count++;
-	}
+    public Logger(String internalDir) {
+        currentDate = LocalDate.now().toString();
+        logDir = LOG_DIR + internalDir + "/";
+        logFile = new File(logDir + currentDate + "-" + count);
+        count++;
+    }
 
-	private void check() {
-		File logDirTest = new File(logDir);
-		if (!logDirTest.exists()) {
-			logDirTest.mkdirs();
-		}
-		if (!currentDate.equals(LocalDate.now().toString())) {
-			currentDate = LocalDate.now().toString();
-		}
-		logFile = new File(logDir + currentDate + "-" + count);
-		if (pw == null) {
-			try {
-				pw = new PrintWriter(logFile);
-			} catch (FileNotFoundException e) {
-				// be sad, nowhere to log :(
-				e.printStackTrace();
-			}
-		}
-	}
+    private void check() {
+        File logDirTest = new File(logDir);
+        if (!logDirTest.exists()) {
+            logDirTest.mkdirs();
+        }
+        if (!currentDate.equals(LocalDate.now().toString())) {
+            currentDate = LocalDate.now().toString();
+        }
+        logFile = new File(logDir + currentDate + "-" + count);
+        if (pw == null) {
+            try {
+                pw = new PrintWriter(logFile);
+            } catch (FileNotFoundException e) {
+                // be sad, nowhere to log :(
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public void logError(Exception e) {
-		check();
-		e.printStackTrace(pw);
-		e.printStackTrace();
-		pw.println();
-		pw.flush();
-	}
+    public void logError(Exception e) {
+        check();
+        e.printStackTrace(pw);
+        e.printStackTrace();
+        pw.println();
+        pw.flush();
+    }
 
-	public void log(String s) {
-		check();
-		pw.println(s);
-		pw.flush();
-	}
+    public void log(String s) {
+        check();
+        pw.println(s);
+        pw.flush();
+    }
 }
