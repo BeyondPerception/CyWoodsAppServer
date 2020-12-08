fdbcb5eaf8cfad2510b32b629e3925aaa45ef922
Ronak Malik <ronakmalik4242@gmail.com>
2020-12-08 16:47:19 -0600

    Add system to update the test user's data using the Testing servlet

--- CyWoodsAppServer/src/main/java/ml/dent/servlet/TestServlet.java
+++ CyWoodsAppServer/src/main/java/ml/dent/servlet/TestServlet.java
@@ -1,47 +1,55 @@
 package ml.dent.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import ml.dent.util.Default;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class TestServlet
  */
-@WebServlet("/loaderio-40a8b5b6364b4626504225e48efa22c0/")
+@WebServlet("/Testing")
 public class TestServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public TestServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		PrintWriter pw = response.getWriter();
-		pw.print("loaderio-40a8b5b6364b4626504225e48efa22c0");
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter pw = response.getWriter();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        pw.println(Default.BadRequest("GET Requests are not allowed!"));
+    }
 
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter pw = response.getWriter();
+
+        String testUserJson = request.getParameter("data");
+
+        if (testUserJson == null) {
+            pw.println(Default.BadRequest("No data provided"));
+            return;
+        }
+        PrintWriter fw = new PrintWriter(new File("/efs/UpdateFiles/TestUser.txt"));
+        fw.println(testUserJson);
+        fw.flush();
+        fw.close();
+        pw.println(Default.OK("Successfully updated test user data"));
+    }
 }
